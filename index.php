8f0cd7fadc2e2555018de1acb7181597e2fe1196
kundankumarKD <40829686+kundankumarKD@users.noreply.github.com>
2025-03-29 17:15:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-// Include configuration
+// Include database configuration
 require_once __DIR__.'/config/Database.php';
 
 // Get request method and URI

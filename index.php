1f382340d38d1def2c1d26ad519b271829568a3c
kundankumarKD <40829686+kundankumarKD@users.noreply.github.com>
2025-03-30 09:25:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+
 // Set headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
@@ -13,9 +14,9 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-// Include database configuration
+// Include configuration
 require_once __DIR__.'/config/Database.php';
-
+include './middlewares/commonMiddleware.php';
 // Get request method and URI
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
@@ -33,7 +34,9 @@ if ($uriSegments[2] === 'products') {
     
     // Get product ID if exists
     $productId = isset($uriSegments[3]) ? (int)$uriSegments[3] : null;
-    
+    $checkIfValideRequest = api_key_middleware();
+
+    if(!$checkIfValideRequest) exit;
     // Initialize and process request
     $controller = new ProductController($db, $requestMethod, $productId);
     $controller->processRequest();
